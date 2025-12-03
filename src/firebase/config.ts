37a62227708f3fa3bde8ebbd14e88e5c658a3473
terryholliday777 <155106059+terryholliday777@users.noreply.github.com>
2025-12-03 04:34:29 -0500

    12.3.25.04.34

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:320090603942:web:4931d8d3b4941cdacfcf67",
   "apiKey": "AIzaSyCY-1umjqW53gDsfJ8kYdpyLuLzCLZBDfg",
   "authDomain": "studio-2235881373-5c755.firebaseapp.com",
+  "storageBucket": "studio-2235881373-5c755.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "320090603942"
 };
