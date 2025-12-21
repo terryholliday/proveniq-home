ef8dc2afc166528fac11ab12445e664e265a7957
Terry Holliday <intuitive.terry@gmail.com>
2025-12-20 21:58:33 -0500

    chore: firebase config update

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,24 @@
+const required = [
+  'NEXT_PUBLIC_FIREBASE_API_KEY',
+  'NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN',
+  'NEXT_PUBLIC_FIREBASE_PROJECT_ID',
+  'NEXT_PUBLIC_FIREBASE_APP_ID',
+  'NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET',
+  'NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID',
+];
+
+for (const key of required) {
+  if (!process.env[key]) {
+    throw new Error(`Missing Firebase config env: ${key}`);
+  }
+}
+
 export const firebaseConfig = {
-  "apiKey": "AIzaSyAndmVF43bZveHHX4iP6OjL-xl1HZjaVdY",
-  "authDomain": "proveniq-home-prod.firebaseapp.com",
-  "projectId": "proveniq-home-prod",
-  "storageBucket": "terryholliday-proveniq-home",
-  "messagingSenderId": "758939868608",
-  "appId": "1:758939868608:web:a060c1a05eddf183027c25",
-  "measurementId": "G-2S7LNTJ8PJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
